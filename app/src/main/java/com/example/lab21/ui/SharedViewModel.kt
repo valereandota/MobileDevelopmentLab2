07328f3b98c460039b82b1c6024f3763268a42c5
Tom Welter <welter.tom@hotmail.com>
2023-04-23 10:00:32 +0200

    not stable and ruining myu life

---
+++ app/src/main/java/com/example/lab21/ui/SharedViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.lab21.ui
+
+class SharedViewModel {
+}
--- app/src/main/java/com/example/lab21/ui/ui/main/MainViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.lab21.ui.ui.main;
-
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
